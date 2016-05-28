12b9bc7071d6fc959170f2473425ba9c7cbf279f
Dumitru Gorgan <dgorgan3@gmail.com>
2016-05-28 17:04:09 -0400

    finished mentor/mentee help category relationships

--- app/models/mentee_help_category.rb
+++ app/models/mentee_help_category.rb
@@ -1,3 +1,4 @@
 class MenteeHelpCategory < ApplicationRecord
-
+  belongs_to :mentee
+  belongs_to :help_category
 end
--- app/models/mentor_help_category.rb
+++ app/models/mentor_help_category.rb
@@ -1,3 +1,4 @@
 class MentorHelpCategory < ApplicationRecord
-
+  belongs_to :mentor
+  belongs_to :help_category
 end
