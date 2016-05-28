5e8680089bae84d7b0f68eb2577b48f49be975d7
Paul Miller <paulmiller@Pauls-MBP.fios-router.home>
2016-05-28 17:24:40 -0400

    Added some controllers and actions

---
+++ app/controllers/course_controller.rb
@@ -0,0 +1,27 @@
+class CourseController < ApplicationController
+  def index
+    render json:
+  end
+
+  def show
+    render json:
+    @course = Course.find
+  end
+
+  def new
+    @course = Course.new(params[:course])
+    if @course.save
+      redirect_to @course
+    else
+      render "new"
+    end
+  end
+
+  def edit
+    @course = Course.edit
+  end
+
+  def save
+    @course = Course.save
+  end
+end
