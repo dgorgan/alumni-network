e2f46c4e8ff8a0e8f7aa11bc593cfcd3e287dac7
Paul Miller <paulmiller@Pauls-MBP.fios-router.home>
2016-05-28 17:07:31 -0400

    removed commented old code

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,8 +1,4 @@
 class Course < ApplicationRecord
-  # belongs_to :mentor_course
-  # has_many :mentees
-  # has_many :mentors
-
   has_many :mentor_courses
   has_many :mentee_courses
   has_many :mentors, through: :mentor_courses
